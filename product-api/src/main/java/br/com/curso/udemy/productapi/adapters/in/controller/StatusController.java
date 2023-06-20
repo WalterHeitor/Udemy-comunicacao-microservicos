151f475037ea81372d64d7c623aca93b35d1c250
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-06-20 15:56:01 -0300

    :truck: create port controller

---
+++ product-api/src/main/java/br/com/curso/udemy/productapi/adapters/in/controller/StatusController.java
@@ -0,0 +1,7 @@
+package br.com.curso.udemy.productapi.adapters.in.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+}

449d32bc4ccf8d544adca750067263fafa4bd9df
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-07-08 01:06:27 -0300

    :sparkles: implement dto

--- product-api/src/main/java/br/com/curso/udemy/productapi/adapters/in/controller/dto/request/CreateCategoryRequest.java
+++ product-api/src/main/java/br/com/curso/udemy/productapi/adapters/in/controller/dto/request/CreateCategoryRequest.java
@@ -2,9 +2,11 @@ package br.com.curso.udemy.productapi.adapters.in.controller.dto.request;
 
 import br.com.curso.udemy.productapi.aplication.core.domain.model.Category;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateCategoryRequest(
         @NotBlank
+        @Size(max = 2000)
         String description
 ) {
     public Category toDomain() {

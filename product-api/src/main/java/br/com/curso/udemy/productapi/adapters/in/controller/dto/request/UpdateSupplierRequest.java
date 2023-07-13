3fcc4cef86632e175dcf7ac984ff5d073feb928e
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-07-13 11:21:26 -0300

    :card_index: implement supplier

--- product-api/src/main/java/br/com/curso/udemy/productapi/adapters/in/controller/dto/request/UpdateSupplierRequest.java
+++ product-api/src/main/java/br/com/curso/udemy/productapi/adapters/in/controller/dto/request/UpdateSupplierRequest.java
@@ -1,12 +1,14 @@
 package br.com.curso.udemy.productapi.adapters.in.controller.dto.request;
 
 import br.com.curso.udemy.productapi.aplication.core.domain.model.Supplier;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record UpdateSupplierRequest (
         @Positive
+        @JsonProperty("supplier_id")
         Long supplierId,
         @NotBlank
         @Size(max = 2000)

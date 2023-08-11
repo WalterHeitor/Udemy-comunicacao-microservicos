109a8c1b209bcfbaffe57b7a1e889b6f17433a0a
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-08-11 15:41:58 -0300

    :card_index: implements client

---
+++ product-api/src/main/java/br/com/curso/udemy/productapi/adapters/out/client/SalesClient.java
@@ -0,0 +1,16 @@
+package br.com.curso.udemy.productapi.adapters.out.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "salesClient",
+        contextId = "salesClient",
+        url = "${app-config.services.sales}"
+)
+public interface SalesClient {
+
+    @GetMapping("/products/{productId")
+    SalesProductResponse findByproductsId(@PathVariable Long productId);
+}
